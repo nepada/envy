ec8ec0d84816b2ad37f2ed4a468348759968b7ea
Petr Morávek <petr@pada.cz>
2020-01-15 20:53:32 +0100

    EnvyExtension: add empty schema

--- src/Bridges/EnvyDI/EnvyExtension.php
+++ src/Bridges/EnvyDI/EnvyExtension.php
@@ -6,10 +6,16 @@ namespace Nepada\Bridges\EnvyDI;
 use Nepada\Envy\Envy;
 use Nepada\Envy\LoaderFactory;
 use Nepada\Envy\ValueProviders\Reader;
+use Nette;
 use Nette\DI\CompilerExtension;
 
 class EnvyExtension extends CompilerExtension
 {
+
+    public function getConfigSchema(): Nette\Schema\Schema
+    {
+        return Nette\Schema\Expect::structure([]);
+    }
 
     public function loadConfiguration(): void
     {
